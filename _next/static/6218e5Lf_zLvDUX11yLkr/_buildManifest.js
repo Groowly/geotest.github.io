0fca4bd8a67bd17ca865aaf7d510e32a2aa1f19a
Groowly <37187584+Groowly@users.noreply.github.com>
2024-07-06 14:38:52 -0600

    Add files via upload

---
+++ _next/static/6218e5Lf_zLvDUX11yLkr/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/39-ce79c5c5e175cf4e.js","static/chunks/pages/index-de80d037988e74db.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
